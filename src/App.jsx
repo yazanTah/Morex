6dd67a0773b7d73a915001902d3257d5e376fe7a
yazanTah <bhjh6562@gmail.com>
2025-02-10 21:03:27 +0300

    fix: update useEffect dependencies to include currentPage; improve accessibility in header text

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,7 @@ const App = () => {
 
   useEffect(() => {
     fetchMovies(debounceSearchTerm, currentPage);
-  }, [debounceSearchTerm]);
+  }, [currentPage, debounceSearchTerm]);
 
   return (
     <Router>
@@ -84,8 +84,8 @@ const App = () => {
                 <header>
                   <img src="./hero.png" alt="Hero Banner" />
                   <h1>
-                    Find <span className="text-gradient">Movies</span> You'll
-                    Enjoy Without the Hassle
+                    Find <span className="text-gradient">Movies</span>{" "}
+                    You&apos;ll Enjoy Without the Hassle
                   </h1>
                   <Search
                     searchTerm={searchTerm}
